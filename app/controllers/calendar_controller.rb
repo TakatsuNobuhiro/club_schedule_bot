2562ff17a83db9ced7c3b79e491a630ce7ed0bef
TakatsuNobuhiro <nobuhiro19980622@gmail.com>
2021-04-26 17:51:49 +0900

    feat calendar model 作成

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -4,15 +4,15 @@ require "googleauth/stores/file_token_store"
 require "date"
 require "fileutils"
 class CalendarController < ApplicationController
-    OOB_URI = ENV["OOBURL"].freeze
+    OOB_URI = ENV["OOB_URI"].freeze
     APPLICATION_NAME = ENV["APPLICATION_NAME"].freeze
-    CREDENTIALS_PATH = "client_secret.json".freeze
+    
     # The file token.yaml stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
     TOKEN_PATH = "token.yaml".freeze
     SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY
-
+    
     ##
     # Ensure valid credentials, either by restoring from the saved credentials
     # files or intitiating an OAuth2 authorization. If authorization is required,
@@ -24,7 +24,7 @@ class CalendarController < ApplicationController
     end 
 
     def authorize
-        client_id = Google::Auth::ClientId.from_file CREDENTIALS_PATH
+        client_id = ENV["CLIENT_ID"]
         
         token_store = Google::Auth::Stores::FileTokenStore.new file: TOKEN_PATH
         authorizer = Google::Auth::UserAuthorizer.new client_id, SCOPE, token_store
@@ -36,7 +36,7 @@ class CalendarController < ApplicationController
             url = authorizer.get_authorization_url base_url: OOB_URI
             puts "Open the following URL in the browser and enter the " \
                 "resulting code after authorization:\n" + url
-            code = session[:code]
+            code = ENV["CODE"]
             
             credentials = authorizer.get_and_store_credentials_from_code(
             user_id: user_id, code: code, base_url: OOB_URI
@@ -48,12 +48,16 @@ class CalendarController < ApplicationController
         credentials
     end
     def callback
+        #urlのcodeをsessionに格納
+
         session[:code] = params[:code]
+        #念の為値をターミナルに吐き出す
         logger.debug(session[:code])
         calendar = Google::Apis::CalendarV3::CalendarService.new
         calendar.client_options.application_name = APPLICATION_NAME
         
         calendar.authorization = authorize
+        #実際のイベントを取得
         fetchEvents(calendar)
         redirect_to action: :index
     end
@@ -65,8 +69,9 @@ class CalendarController < ApplicationController
     end
 
     def fetchEvents(service)
+        
         # Fetch the next 10 events for the user
-        calendar_id = "4relcv3achthpmqe5lb88944bs@group.calendar.google.com"
+        calendar_id = ENV["CALENDAR_ID"]
         response = service.list_events(calendar_id,
                                     max_results:   10,
                                     single_events: true,
