f1f9ad1d9f8119dfb2c67bdc80b748e560f5cd00
TakatsuNobuhiro <nobuhiro19980622@gmail.com>
2021-04-30 14:41:53 +0900

    fix broad_push.rbの内容をmessage.rbに移す

--- app/models/message.rb
+++ app/models/message.rb
@@ -16,4 +16,28 @@ class Message
             result = "明日は#{start_time}から#{end_time}まで#{location}#{title}があります。\n欠席or遅刻者は背番号＋（スペース）遅刻or欠席+（スペース）理由の形式でご回答ください。\n(例)21番 欠席 授業があるため"
         end
     end
+
+    def broad_push
+        require 'net/http'
+        require 'uri'
+        require 'json' 
+
+        token = ENV["LINE_CHANNEL_TOKEN"]
+        # post先のurl
+        uri = URI.parse('https://api.line.me/v2/bot/message/broadcast')
+        http = Net::HTTP.new(uri.host,uri.port)
+        http.use_ssl = true
+
+        # Header
+        headers = {
+            'Authorization'=>"Bearer #{token}",
+            'Content-Type' =>'application/json',
+            'Accept'=>'application/json'
+        }
+        send_message = self.organize_from_calendar
+        # Body
+        params = {"messages" => [{"type" => "text", "text" => send_message}]}
+
+        response = http.post(uri.path, params.to_json, headers)
+    end
 end
